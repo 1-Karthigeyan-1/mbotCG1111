f422bce3544302fb777924b23877f7f89efec629
kaiwen98 <57187325+kaiwen98@users.noreply.github.com>
2019-11-16 17:45:19 +0800

    Update Sound_draft_1.ino

--- Sound_draft_1.ino
+++ Sound_draft_1.ino
@@ -1,62 +1,40 @@
-#define MICBP 4
-#define MICHP 3
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(MICBP, INPUT);
-  pinMode(MICHP, INPUT);
-}
-
-void loop(){
-  float valBP = analogRead(MICBP)/1023.0*5;
-  float valHP = analogRead(MICHP)/1023.0*5;
-  /*Serial.print("Voltage of sumBP: "); Serial.print(valBP); Serial.print("  "); //BP
-  Serial.print("Voltage of sumHP: "); Serial.print(valHP); Serial.print("  "); //BP
-  Serial.println("uT");*/
-  sound();
-  delay(100);
-}
-
-void sound() {
-  float valBP;
-  float valHP;
+int checkSound()
+{
+  Serial.println("Checking Sound");
+  float valBP;                        //Bandpass valuw
+  float valHP;                        //Highpass value
   float maxBP = 0, maxHP = 0;
   int count = 0;
-  float diff=0;
-  
-  
-  while(count < 600){ //I set it to run for around 3 seconds to sample at least the whole duration of one song cycle of each song
-      valBP = analogRead(MICBP)/1023.0*5;
-      valHP = analogRead(MICHP)/1023.0*5;
-    if (valBP > maxBP){
+  float diff = 0;
+
+  //run for around 3 seconds to sample at least the whole duration of one song cycle of each song
+  int time = millis();
+  while ((millis() - time) < 3000) { 
+    time = millis();
+    valBP = Sound.aRead1()/ 1023.0 * 5;       //Get low pass values  
+    valHP = Sound.aRead2()/ 1023.0 * 5;       //Get high pass values
+    
+    if (valBP > maxBP) {                      //Finds the maximum bandpass value
       maxBP = valBP;
     }
 
-    if (valHP > maxHP){
-      maxHP = valHP;
+    if (valHP > maxHP) {                      //Finds the maximum highpass value
+      maxHP = valHP;                          
     }
-    //Serial.print("Voltage of valBP: "); Serial.print(valBP); Serial.println("  "); //BP
-    //Serial.print("Voltage of valHP: "); Serial.print(valHP); Serial.println("  "); //BP
-    //Serial.print("Voltage of diff: "); Serial.print(maxBP - maxHP); Serial.println("  "); //BP
-    count++;
-    delay(10);
+    delayMicroseconds(250);                   //
+   
   }
-  if((maxBP - maxHP) > 0.15){
-    Serial.println("Frequency is 100 to 300!");
-    //movement(LEFT);
-    
+  
+  if ((maxBP - maxHP) > BP_threshold) {       //Indicates that the frequency is below 100-300Hz
+    return LEFT_90;
+
   }
-  else if ((maxHP - maxBP) > 0.15){
-    Serial.println("Frequency is above 3000!");
-    //movement(right);
+  else if ((maxHP - maxBP) > HP_threshold) {  //Indicates that the frquencyt is above 300Hz
+    return RIGHT_90;
   }
-  else{
-    Serial.println("Your mom is gay");
+           
+  else {
     return 0;
   }
-  Serial.print("Voltage of sumBP: "); Serial.print(maxBP); Serial.print("  "); 
-  Serial.println("Voltage of sumHP: "); Serial.print(maxHP); Serial.print("  "); 
-  Serial.println("Voltage of diff: "); Serial.print(maxBP - maxHP); Serial.print("  ");
-  return 1;
+
 }
